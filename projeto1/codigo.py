31a6ba08ab875788436a0520718f2150cf9d1bdc
Tiago <150839960+Tiago-DSS@users.noreply.github.com>
2026-01-14 00:50:34 -0300

    Add files via upload
    Este projeto automatiza o preenchimento de dados em um site a partir de uma planilha .csv. No exemplo, os dados representam produtos de uma empresa fictícia, mas a automação pode ser adaptada para outros tipos de informações, alterando apenas alguns parâmetros do script.
    
    Como próximo passo, pretendo melhorar a automação para que os cliques não dependam da resolução da tela, tornando o código mais flexível para diferentes ambientes.

---
+++ projeto1/codigo.py
@@ -0,0 +1,67 @@
+#objetivo: 
+#abrir o site da empresa, preencher o email e a senha
+#colocar as informações corretas dos produtos no site
+#e continuar preenchendo até acabar os dados
+
+import pandas #API para manipulação de dados
+import pyautogui #API para automações
+import time #API para trabalhar com tempo
+
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+email = "emailboladao@gmail.com"
+
+pyautogui.PAUSE = 1 # para dar um tempo de uma ação para outra
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.write(link)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.click(x=780, y=372)# clicar no campo do email
+pyautogui.write(email)
+pyautogui.press("tab")
+pyautogui.write("senha linda")
+pyautogui.press("enter")
+time.sleep(1)
+
+#pandas vai ler o arquivo csv
+#tambem daria para no excel: pandas.read_excel("produtos.xlsx") ou colocando o caminho completo até a planilha
+tabela = pandas.read_csv("C:/projetos_programacao/vscode/teste_python/CURSO_GRATIS_HASHTAG/projeto1/produtos.csv")
+
+#vai passar por cada linha da tabela
+#a variavel linha foi criada devido o .index, se fosse coluna seria .columns
+#o passo de agora é fazer com que o pyautogui cadastre os produtos no site
+for linha in tabela.index:
+    pyautogui.click(x=754, y=262) #clica no botão de novo produto
+    codigo = tabela.loc[linha, "codigo"]
+    time.sleep(0.3)
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(str(marca)) 
+    pyautogui.press("tab") 
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(str(tipo))
+    pyautogui.press("tab")
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+    obs = str(tabela.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+    pyautogui.press("enter")
+    pyautogui.hotkey("ctrl, home") #volta para o topo da página
+    time.sleep(0.5)
+
+
+
+#resumindo o que esse código acima faz:
+#
---
+++ projeto1/descobrir_posi_click.py
@@ -0,0 +1,7 @@
+# descobrir a posição do mouse na tela
+
+import time
+import pyautogui
+
+time.sleep(5)
+print(pyautogui.position())
